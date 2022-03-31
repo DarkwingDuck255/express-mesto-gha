800869074d88ade85aa42185518557e84509dfde
BlackCloak <ars081190@yandex.ru>
2022-03-31 22:16:28 +0300

    мелкие правки

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,31 +1,10 @@
 const allowedCors = [
+  'http://localhost:3000',
   'https://api.darkwingduck.nomoredomains.xyz',
   'http://api.darkwingduck.nomoredomains.xyz',
-  'http://localhost:3000',
   'https://darkwingduck.nomoredomains.work',
   'http://darkwingduck.nomoredomains.work',
 ];
-
-// const cors = (req, res, next) => {
-//   const { method } = req;
-//   const { origin } = req.headers;
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   const requestHeaders = req.headers['access-control-request-headers'];
-
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', true);
-//   }
-
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-//   return next();
-// };
-
-// module.exports = cors;
 
 const cors = (req, res, next) => {
   const { origin } = req.headers;
