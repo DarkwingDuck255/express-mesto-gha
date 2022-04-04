07bdd1185c1f0469ebf1747fd7297dd3a92b365e
BlackCloak <ars081190@yandex.ru>
2022-04-04 21:58:37 +0300

    cors again

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
